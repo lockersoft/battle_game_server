a7b2c19a35819bab577e250055aaa39998cfcb8a
lockersoft <dave@lockersoft.com>
2013-12-22 23:01:43 -0800

    fixed spacing in readme

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-BattleGameServer::Application.config.session_store :cookie_store, key: '_battleship_server_session'
+BattleGameServer::Application.config.session_store :cookie_store, key: '_battle_game_server_session'
